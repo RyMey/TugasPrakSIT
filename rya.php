3a9005944bbc0cefe237db08d7e1cba9c234112d
Rya Meyvriska <rya.meyvriska@gmail.com>
2015-09-03 14:09:53 +0700

    First commit, initial nama dan nim

---
+++ rya.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User           : RyMey
+ * Email          : rya.meyvriska@mail.ugm.ac.id
+ * Linkedin       : https://id.linkedin.com/in/rymey
+ * Github Account : https://github.com/RyMey
+ * Date           : 03/09/2015
+ * Time           : 14.01
+ */
+
+ echo "Rya Meyvriska";
+ echo "<br/>";
+ echo "13/354645/SV/04762";
+?>
